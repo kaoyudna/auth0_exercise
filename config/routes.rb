87c403ce0b7f8d97845739c6a0c0b32ed563c19b
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-04-21 02:52:54 +0000

    [Add]

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'dashboard', to: 'dashboard#show'
+
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
+end
